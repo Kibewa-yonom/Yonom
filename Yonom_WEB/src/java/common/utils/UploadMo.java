a8f4828f4ee66546755458a5a030d1712be03708
Jeong Yong Gyu <59310399+Yonggyu-Jeong@users.noreply.github.com>
2022-07-05 13:22:16 +0900

    Migration from other projects
    Migration from other projects

---
+++ Yonom_WEB/src/java/common/utils/UploadMo.java
@@ -0,0 +1,17 @@
+package common.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import common.collection.ALog;
+import common.collection.ASession;
+import common.dao.CommonDaoIF;
+
+/**
+ * <pre>
+ * 	모든 Service 클래스의 부모클래스로써 공통기능 및 멤버 변수를 선언함.
+ * </pre>
+ */
+public class UploadMo{
+
+
+}
